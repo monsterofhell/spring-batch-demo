ac6d54cab5f39d5430ed9fe7b1a463eb0149935d
Chris Morris <morris007chris1@gmail.com>
2021-11-02 15:15:37 +0530

    Spring Initializer

---
+++ spring-batch-demo/src/test/java/com/batch/spring/springbatchdemo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.spring.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
